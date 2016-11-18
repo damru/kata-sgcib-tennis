f815b4c2509a2f7f063bc32f2b7fdb311124ca5a
Damien Rubio <rubio.damien@gmail.com>
2016-11-18 08:05:58 +0100

    initiate architecture and test

---
+++ src/main/java/com/damru/kata/sgcib/tennis/models/Joueur.java
@@ -0,0 +1,7 @@
+package com.damru.kata.sgcib.tennis.models;
+
+/**
+ * Created by damien on 18/11/2016.
+ */
+public class Joueur {
+}
---
+++ src/main/java/com/damru/kata/sgcib/tennis/services/MatchService.java
@@ -0,0 +1,15 @@
+package com.damru.kata.sgcib.tennis.services;
+
+import org.springframework.stereotype.Service;
+
+import static java.lang.System.out;
+
+/**
+ * Created by damien on 17/11/2016.
+ */
+@Service
+public class MatchService {
+    public void getScore() {
+        out.println("Hello");
+    }
+}
