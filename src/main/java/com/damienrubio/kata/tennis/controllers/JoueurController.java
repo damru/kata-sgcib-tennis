6ed806dda0bca040982b5c8b20ef7922dfb0babd
Damien Rubio <rubio.damien@gmail.com>
2017-03-04 12:41:51 +0100

    Add Swagger and correct JAX-RS implementation

--- src/main/java/com/damienrubio/kata/tennis/controllers/JoueurController.java
+++ src/main/java/com/damienrubio/kata/tennis/controllers/JoueurController.java
@@ -2,38 +2,61 @@ package com.damienrubio.kata.tennis.controllers;
 
 import com.damienrubio.kata.tennis.models.Joueur;
 import com.damienrubio.kata.tennis.services.MatchService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  * Created by damien on 21/11/2016.
  */
 @RestController
+@Api(value = "/joueur",
+     description = "Gestion des joueurs.")
+@Produces(MediaType.APPLICATION_JSON)
 @Path("/joueur")
+@Slf4j
 public class JoueurController {
 
     @Autowired
     private MatchService matchService;
 
-    @Path("/creer")
+    @ApiOperation(value = "Créer un nouveau joueur",
+                  notes = "Retourne un nouveau joueur",
+                  response = Joueur.class)
+    @ApiResponse(code = 200,
+                 message = "Le joueur est créé")
     @POST
     @Produces(MediaType.APPLICATION_JSON)
-    public Joueur creer(@QueryParam("nom") String nom, @QueryParam("prenom") String prenom) {
-        return new Joueur(nom, prenom);
+    @Path("/creer")
+    public Response creer(@ApiParam("Nom du joueur à créer") @QueryParam("nom") String nom,
+                          @ApiParam("Prénom du joueur à créer") @QueryParam("prenom") String prenom) {
+        return Response.ok(new Joueur(nom, prenom)).build();
     }
 
-    @Path("/{joueurId}")
+    @ApiOperation(value = "Afficher un joueur",
+                  notes = "Affiche un joueur",
+                  response = Joueur.class)
+    @ApiResponse(code = 200,
+                 message = "Le joueur est affiché")
     @POST
     @Produces(MediaType.APPLICATION_JSON)
-    public Joueur afficher(@RequestBody Joueur joueur) {
-        return joueur;
+    @Path("/{joueurId}")
+    public Response afficher(@ApiParam("Id du joueur à afficher") @PathParam("joueurId") Long joueurId,
+                             @ApiParam("Joueur à afficher") @RequestBody Joueur joueur) {
+        return Response.ok(joueur).build();
     }
 
 }
--- src/main/java/com/damienrubio/kata/tennis/controllers/MatchController.java
+++ src/main/java/com/damienrubio/kata/tennis/controllers/MatchController.java
@@ -3,6 +3,11 @@ package com.damienrubio.kata.tennis.controllers;
 import com.damienrubio.kata.tennis.models.Joueur;
 import com.damienrubio.kata.tennis.models.Match;
 import com.damienrubio.kata.tennis.services.MatchService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,13 +15,21 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  * Created by damien on 21/11/2016.
  */
+@Api(value = "/match",
+     description = "Gestion des matchs.")
+@Produces(MediaType.APPLICATION_JSON)
 @RestController
 @Path("/match")
+@Slf4j
 public class MatchController {
 
     @Autowired
@@ -27,24 +40,46 @@ public class MatchController {
         return "match controller";
     }
 
+    @ApiOperation(value = "Créer un nouveau match",
+                  notes = "Retourne un nouveau match",
+                  response = Match.class)
+    @ApiResponse(code = 200,
+                 message = "Le match est créé")
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
     @Path(value = "/creer")
-    @POST
-    public Match creer(@RequestBody Joueur joueur1, @RequestBody Joueur joueur2,
-                       @QueryParam("nbSets") @DefaultValue("2") int nbSetsGagnants) {
-        return new Match(joueur1, joueur2, nbSetsGagnants);
+    public Response creer(@ApiParam("Joueur1 du match à créer") @RequestBody Joueur joueur1,
+                          @ApiParam("Joueur2 du match à créer") @RequestBody Joueur joueur2,
+                          @ApiParam("Nombre de sets nécessaires pour gagner le match") @QueryParam("nbSets") @DefaultValue("2")
+                              int nbSetsGagnants) {
+        return Response.ok(new Match(joueur1, joueur2, nbSetsGagnants)).build();
     }
 
+    @ApiOperation(value = "Afficher un match",
+                  notes = "Affiche un match",
+                  response = Match.class)
+    @ApiResponse(code = 200,
+                 message = "Le match est créé")
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
     @Path(value = "/{matchId}")
-    @POST
-    public Match afficher(@RequestBody Match match) {
-        return match;
+    public Response afficher(@ApiParam("Id du match à afficher") @PathParam("matchId") Long matchId,
+                             @ApiParam("Match à afficher") @RequestBody Match match) {
+        return Response.ok(match).build();
     }
 
-    @Path(value = "/marquer")
+    @ApiOperation(value = "Un joueur marque un point dans le match",
+                  notes = "Le joueur marque",
+                  response = Match.class)
+    @ApiResponse(code = 200,
+                 message = "Le joueur a marqué un point")
     @POST
-    public Match marquer(@RequestBody Match match, @RequestBody Joueur joueur) {
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path(value = "/marquer")
+    public Response marquer(@ApiParam("Match dans lequel un point est marqué") @RequestBody Match match,
+                            @ApiParam("Joueur marquant le point") @RequestBody Joueur joueur) {
         matchService.marquerPoint(match, joueur);
-        return match;
+        return Response.ok(match).build();
     }
 
 }
